c621d6787f6940bcd50ba36669800a68caf899b9
dzmitry <eqwewqdasdsaczxcxz@gmail.com>
2018-08-30 17:00:54 -0400

    blink on interrupt

--- 16f676_8b_led_blink.asm
+++ 16f676_8b_led_blink.asm
@@ -3,40 +3,46 @@
 	include "p16f676.inc"	;include the defaults for the chip
 	__config 0x3D14			;sets the configuration settings (oscillator type etc.)
 							; HERE SET TO INTERNAL OSCILLATOR 4MHZ
-
+							
 	cblock 	0x20 			;start of general purpose registers
 		count1 			;used in delay routine
 		counta 			;used in delay routine 
 		countb 			;used in delay routine
 	endc
+
+	org	0x004
+		goto	Interrupt
 	
-	org	0x0000			;org sets the origin, 0x0000 for the 16F628,
+	org	0x0050			;org sets the origin, 0x0000 for the 16F628,
 					;this is where the program starts running	
 
 	bcf 	STATUS,RP0 	;Bank 0
 	clrf 	PORTA 		;Init PORTA
-	movlw	05h 		;Set RA<2:0> to 
+	clrf	PORTC
+	movlw	0xd8
+	movwf	INTCON		;interrupts enable
+	movlw	0x3f 		;Set RA<5:0> to 
 	movwf	CMCON 		;digital I/O
+	movlw	0x10
+	movwf	IOCA		;enable interrupt on change
 	bsf 	STATUS,RP0 	;Bank 1
 	clrf	ANSEL 		;digital I/O
-	movlw 	00h 		; 
-	movwf 	TRISA 		;as outputs
+	movlw 	0x3f 		; 
+	movwf 	TRISA 		;as inputs
 
-   	movlw 	b'00000000'	;set PortC all outputs
-   	movwf 	TRISC
-	movwf	TRISA		;set PortA all outputs
+	movlw	b'00000000'
+	movwf	TRISC   	
+	
 	bcf		STATUS,	RP0	;select bank 0
+	
+	
+	;bcf	STATUS,T0CS	;setting timer operation mode as timer
 
 Loop	
-	movlw	0xff
-	movwf	PORTA			;set all bits on
-	movwf	PORTC
 	nop				;the nop's make up the time taken by the goto
 	nop				;giving a square wave output
-	call	Delay			;this waits for a while!
-	movlw	0x00
-	movwf	PORTA
-	movwf	PORTC			;set all bits off
+	;movlw	0xff
+	;xorwf	PORTC,1
 	call	Delay
 	goto	Loop			;go back and do it again
 
@@ -55,4 +61,11 @@ Delay_0
 	goto	d1
 	retlw	0x00
 
+Interrupt
+	movlw	0xff
+	xorwf	PORTC,1
+	movlw	0xd8
+	movwf	INTCON
+	return
+	
 	end
