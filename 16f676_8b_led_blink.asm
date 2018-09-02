944aea518f7f489cae8e83f62499609289057fca
dzmitry <eqwewqdasdsaczxcxz@gmail.com>
2018-09-02 13:44:21 -0400

    7 - segment indicator counting

--- 16f676_8b_led_blink.asm
+++ 16f676_8b_led_blink.asm
@@ -8,42 +8,83 @@
 		count1 			;used in delay routine
 		counta 			;used in delay routine 
 		countb 			;used in delay routine
+		number
+		oneA
+		oneC
+		twoA
+		twoC
 	endc
 
-	org	0x004
-		goto	Interrupt
+	;org	0x004
+	;	goto	Interrupt
 	
 	org	0x0050			;org sets the origin, 0x0000 for the 16F628,
 					;this is where the program starts running	
 
 	bcf 	STATUS,RP0 	;Bank 0
 	clrf 	PORTA 		;Init PORTA
+	MOVLW	0x3f
+	movwf	CMCON
 	clrf	PORTC
-	movlw	0xd8
-	movwf	INTCON		;interrupts enable
 	movlw	0x3f 		;Set RA<5:0> to 
 	movwf	CMCON 		;digital I/O
-	movlw	0x10
-	movwf	IOCA		;enable interrupt on change
 	bsf 	STATUS,RP0 	;Bank 1
 	clrf	ANSEL 		;digital I/O
-	movlw 	0x3f 		; 
-	movwf 	TRISA 		;as inputs
-
-	movlw	b'00000000'
-	movwf	TRISC   	
+	movlw 	0x00 		; 
+	movwf 	TRISA 		;as outputs
+	movwf 	TRISC 		;as outputs
 	
 	bcf		STATUS,	RP0	;select bank 0
 	
+	;Initialize number constants
+	movlw	0x3f	;0
+	movwf	0x30
+	movlw	0x00
+	movwf	0x40	
+	movlw	0x21	;1
+	movwf	0x31	
+	movlw	0x00
+	movwf	0x41	
+	movlw	0x1B	;2
+	movwf	0x32
+	movlw	0x01
+	movwf	0x42	
+	movlw	0x33	;3
+	movwf	0x33
+	movlw	0x01
+	movwf	0x43	
+	movlw	0x29	;4
+	movwf	0x34
+	movlw	0x01
+	movwf	0x44	
+	movlw	0x3f	;8
+	movwf	0x38
+	movlw	0x01
+	movwf	0x48	
 	
+	movlw	0x00
+	movwf	number
 	;bcf	STATUS,T0CS	;setting timer operation mode as timer
 
 Loop	
 	nop				;the nop's make up the time taken by the goto
 	nop				;giving a square wave output
-	;movlw	0xff
-	;xorwf	PORTC,1
+	movlw	0x00
+	movwf	PORTC
+	movwf	PORTA
 	call	Delay
+	
+	call	RenderNumber
+
+	call	Delay
+	
+	movfw	number
+	sublw	d'4'
+	btfsc	STATUS,Z
+	movwf	number
+	
+	incf	number,1
+	
 	goto	Loop			;go back and do it again
 
 Delay	movlw	d'250'			;delay 250 ms (4 MHz clock)
@@ -60,12 +101,20 @@ Delay_0
 	decfsz	count1	,f
 	goto	d1
 	retlw	0x00
-
-Interrupt
-	movlw	0xff
-	xorwf	PORTC,1
-	movlw	0xd8
-	movwf	INTCON
-	return
 	
+RenderNumber
+	movlw	0x30
+	addwf	number,0
+	movwf	FSR
+	movfw	INDF
+	movwf	PORTC
+	movlw	0x40
+	addwf	number,0
+	movwf	FSR
+	btfsc	INDF,0
+	bsf	PORTA,0
+	btfss	INDF,0
+	bcf	PORTA,0
+	bcf	PORTA,5
+	retlw	0x00
 	end
