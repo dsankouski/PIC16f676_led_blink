ecf4d44693bafff09256f2adad48cca3f4895e4d
Dzmitry Sankouski <dsankouski@eisgroup.com>
2018-09-25 20:04:57 +0300

    blinking with timer0

--- 16f676_8b_led_blink.asm
+++ 16f676_8b_led_blink.asm
@@ -1,58 +1,69 @@
-;Tutorial 1.2 - Nigel Goodwin 2002  ; MODIFIED FOR PIC16F676
-
+	; ----------------------------------------------------------------------------
+	; "THE BEER-WARE LICENSE" (Revision 42):
+	; dsankouski@gmail.com wrote this file. As long as you retain this notice you
+	; can do whatever you want with this stuff. If we meet some day, and you think
+	; this stuff is worth it, you can buy me a beer in return. Dzmitry Sankouski
+	; ----------------------------------------------------------------------------
+	
+	; This is a sample blinking LED project. It uses on chip 8 bit timer with 
+	; 8 bit prescaler to create blinking period. All controller pins
+	; (both portA and PortC) configured as outputs. When timer fires, all pins
+	; invert state. Blinking period will be approx. 1sec.
+	
 	include "p16f676.inc"	;include the defaults for the chip
 	__config 0x3D14			;sets the configuration settings (oscillator type etc.)
 							; HERE SET TO INTERNAL OSCILLATOR 4MHZ
 
-	cblock 	0x20 			;start of general purpose registers
-		count1 			;used in delay routine
-		counta 			;used in delay routine 
-		countb 			;used in delay routine
+	cblock 	0x20			;start of general purpose registers
+	    light_state
 	endc
 	
-	org	0x0000			;org sets the origin, 0x0000 for the 16F628,
-					;this is where the program starts running	
+	org	0x0004				;interrupt vector
+		goto	InterruptHandler
+	
+	org	0x0050				;org sets the origin, 0x0000 for the 16F628,
+							;this is where the program starts running	
 
-	bcf 	STATUS,RP0 	;Bank 0
-	clrf 	PORTA 		;Init PORTA
-	movlw	05h 		;Set RA<2:0> to 
-	movwf	CMCON 		;digital I/O
-	bsf 	STATUS,RP0 	;Bank 1
-	clrf	ANSEL 		;digital I/O
-	movlw 	00h 		; 
-	movwf 	TRISA 		;as outputs
-
-   	movlw 	b'00000000'	;set PortC all outputs
-   	movwf 	TRISC
-	movwf	TRISA		;set PortA all outputs
-	bcf		STATUS,	RP0	;select bank 0
+	bcf 	STATUS,RP0		;Bank 0
+	clrf 	PORTA			;Init PORTA
+	bsf 	STATUS,RP0		;Bank 1
+	clrf	ANSEL			;digital I/O
+   	clrf 	TRISC			;set PortC all outputs to 0
+	clrf	TRISA		
+	bcf		STATUS,	RP0		;select bank 0
+	
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	;;;;; Timer setup. Registers TMR0 , INTCON, OPTION_REG
+	clrf	INTCON
+	bsf		INTCON,GIE		;enabling global interrupt flag
+	bsf		INTCON,T0IE		;enabling interrupt for timer 0
+	clrf	TMR0
+	bsf		STATUS,RP0		;Bank 1
+	;Prescaler setup
+	;configuring prescaler, and assigning prescaler to timer0
+	;scaling is 256. Timer frequency is Fosc/(4*2*65536)
+	movlw   b'0000111'
+	movwf	OPTION_REG
+	;;;;;;End of scaling setup
+	bcf		STATUS,	RP0		;select bank 0
+	;;;;;;End of timer setup
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 Loop	
-	movlw	0xff
-	movwf	PORTA			;set all bits on
-	movwf	PORTC
-	nop				;the nop's make up the time taken by the goto
-	nop				;giving a square wave output
-	call	Delay			;this waits for a while!
-	movlw	0x00
-	movwf	PORTA
-	movwf	PORTC			;set all bits off
-	call	Delay
-	goto	Loop			;go back and do it again
+	nop				
+	nop				
+	goto	Loop			
 
-Delay	movlw	d'250'			;delay 250 ms (4 MHz clock)
-	movwf	count1
-d1	movlw	0xC7
-	movwf	counta
-	movlw	0x01
-	movwf	countb
-Delay_0
-	decfsz	counta, f
-	goto	$+2
-	decfsz	countb, f
-	goto	Delay_0
-	decfsz	count1	,f
-	goto	d1
-	retlw	0x00
-
-	end
+InterruptHandler
+    incf    light_state		;increment light state
+    btfsc   light_state,3	;additional scaling by 8.
+    movlw   0x3f			;if light state 3 bit is 0, turn on leds
+    btfss   light_state,3
+    movlw   0x00			;if light state 3 bit is 1, turn off leds	
+    movwf   PORTA
+    movwf   PORTC
+    clrf	TMR0			;resetting timer
+	bcf		INTCON,T0IF		;clearing timer0 overflow bit
+    retfie
+	
+end
