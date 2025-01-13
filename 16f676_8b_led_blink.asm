42b0982d4f90562f98e3ca1196958f30c1481bcb
Dzmitry Sankouski <dsankouski@gmail.com>
2025-01-13 17:38:59 +0300

    format code

--- 16f676_8b_led_blink.asm
+++ 16f676_8b_led_blink.asm
@@ -1,4 +1,4 @@
-	; ----------------------------------------------------------------------------
+; ----------------------------------------------------------------------------
 	; "THE BEER-WARE LICENSE" (Revision 42):
 	; dsankouski@gmail.com wrote this file. As long as you retain this notice you
 	; can do whatever you want with this stuff. If we meet some day, and you think
@@ -15,7 +15,7 @@
 							; HERE SET TO INTERNAL OSCILLATOR 4MHZ
 
 	cblock 	0x20			;start of general purpose registers
-	    light_state
+		light_state
 	endc
 	
 	org	0x0004				;interrupt vector
@@ -28,7 +28,7 @@
 	clrf 	PORTA			;Init PORTA
 	bsf 	STATUS,RP0		;Bank 1
 	clrf	ANSEL			;digital I/O
-   	clrf 	TRISC			;set PortC all outputs to 0
+	clrf 	TRISC			;set PortC all outputs to 0
 	clrf	TRISA		
 	bcf		STATUS,	RP0		;select bank 0
 	
@@ -42,7 +42,7 @@
 	;Prescaler setup
 	;configuring prescaler, and assigning prescaler to timer0
 	;scaling is 256. Timer frequency is Fosc/(4*2*65536)
-	movlw   b'0000111'
+	movlw	 b'0000111'
 	movwf	OPTION_REG
 	;;;;;;End of scaling setup
 	bcf		STATUS,	RP0		;select bank 0
@@ -55,15 +55,15 @@ Loop
 	goto	Loop			
 
 InterruptHandler
-    incf    light_state		;increment light state
-    btfsc   light_state,3	;additional scaling by 8.
-    movlw   0x3f			;if light state 3 bit is 0, turn on leds
-    btfss   light_state,3
-    movlw   0x00			;if light state 3 bit is 1, turn off leds	
-    movwf   PORTA
-    movwf   PORTC
-    clrf	TMR0			;resetting timer
-	bcf		INTCON,T0IF		;clearing timer0 overflow bit
-    retfie
-	
+	incf		light_state		;increment light state
+	btfsc	 light_state,3	;additional scaling by 8.
+	movlw	 0x3f			;if light state 3 bit is 0, turn on leds
+	btfss	 light_state,3
+	movlw	 0x00			;if light state 3 bit is 1, turn off leds	
+	movwf	 PORTA
+	movwf	 PORTC
+	clrf	TMR0			;resetting timer
+  bcf		INTCON,T0IF		;clearing timer0 overflow bit
+	retfie
 end
+
